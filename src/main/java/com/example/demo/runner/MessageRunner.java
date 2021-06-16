48ea474d5a246f9261e0218adad38da91b19bd88
0015OK744 <0015OK744@9.85.83.220>
2021-06-16 13:38:33 +0530

    First Time Updating a File

---
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -0,0 +1,16 @@
+package com.example.demo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Application Testing...");
+		
+	}
+
+}
