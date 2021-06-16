d0868b04ff8ef134fed127e9aaf59fe84c2f2713
0015OK744 <0015OK744@9.85.83.220>
2021-06-16 14:16:21 +0530

    123 commit to next

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -9,8 +9,9 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		int a=10;
 		System.out.println("Application Testing...");
-		System.out.println("Applying some modifications");
+		System.out.println("Applying some modifications "+10);
 	}
 
 }
